e06cc9d5f49a618b5db16d0cfc227dd3847a098d
coolchenshan <coolchenshan@gmail.com>
2013-09-17 10:31:12 +0800

    (Log base 2 approximation and Newton's Method) + (Babylonian Method + some manipulations on IEEE 32 bit floating point representation) + (Newton's Approximation Method)

--- q_rsqrt.c
+++ q_rsqrt.c
@@ -1,7 +1,12 @@
 /* http://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi*/
 
 #include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#define ACCURACY 0.001
+
+// Log base 2 approximation and Newton's Method
 float sqrt1(const float x)
 {
     union
@@ -15,6 +20,7 @@ float sqrt1(const float x)
     return u.x;
 }
 
+// Babylonian Method + some manipulations on IEEE 32 bit floating point representation
 float sqrt2(const float x)
 {
     union
@@ -34,11 +40,68 @@ float sqrt2(const float x)
     return u.x;
 }
 
+// Newton's Approximation Method
+double sqrt3( unsigned long N )
+ {
+     double n, p, low, high;
+     if( 2 > N )
+         return( N );
+     low  = 0;
+     high = N;
+     while( high > low + 1 )
+     {
+         n = (high + low) / 2;
+         p = n * n;
+         if( N < p )
+             high = n;
+         else if( N > p )
+             low = n;
+         else
+             break;
+     }
+     return( N == p ? n : low );
+ }
+
+// http://blog.csdn.net/wzy_1988/article/details/11758049
+double sqrt4(double n)  
+{  
+    double low, high, mid, tmp;  
+  
+    // 获取上下界  
+    if (n > 1)   {  
+        low = 1;  
+        high = n;  
+    } else {  
+        low = n;  
+        high = 1;  
+    }  
+  
+    // 二分法求开方  
+    while (low <= high) {  
+        mid = (low + high) / 2.000;  
+  
+        tmp = mid * mid;  
+  
+        if (abs(tmp - n) <= ACCURACY) {  
+            return mid;  
+        } else if (tmp > n) {  
+            high = mid;  
+        } else {  
+            low = mid;  
+        }  
+    }  
+  
+    return -1.000;  
+}
+
 void main() {
-    float cout1, cout2, cin = 3;
+    float cin = 3;
+    float cout1, cout2, cout3, cout4;
 
     cout1 = sqrt1(cin);
     cout2 = sqrt2(cin);
+    cout3 = sqrt3(cin);
+    cout4 = sqrt4(cin);
 
-    printf("%f %f \n", cout1, cout2);
+    printf("%f %f %f %f\n", cout1, cout2, cout3, cout4);
 }
