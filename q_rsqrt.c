cbe9e5d99ba84c48a02a0e92d43bbb3f87b3d1ba
coolchenshan <coolchenshan@gmail.com>
2013-09-17 09:43:50 +0800

    sqrt implemented by Newton's Method and Babylonian Method.

--- q_rsqrt.c
+++ q_rsqrt.c
@@ -2,24 +2,20 @@
 
 #include<stdio.h>
 
-/* float Q_rsqrt( float number ) */
-/* { */
-/*     long i; */
-/*     float x2, y; */
-/*     const float threehalfs = 1.5F; */
-
-/*     x2 = number * 0.5F; */
-/*     y  = number; */
-/*     i  = * ( long * ) &y;                       // evil floating point bit level hacking（对浮点数的邪恶位级hack） */
-/*     i  = 0x5f3759df - ( i >> 1 );               // what the fuck?（这他妈的是怎么回事？） */
-/*     y  = * ( float * ) &i; */
-/*     y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration （第一次牛顿迭代） */
-/*     //      y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed（第二次迭代，可以删除） */
-
-/*     return y; */
-/* } */
-
 float sqrt1(const float x)
+{
+    union
+    {
+        int i;
+        float x;
+    } u;
+
+    u.x = x;
+    u.i = (1<<29) + (u.i >> 1) - (1<<22);
+    return u.x;
+}
+
+float sqrt2(const float x)
 {
     union
     {
@@ -39,9 +35,10 @@ float sqrt1(const float x)
 }
 
 void main() {
-    float cout, cin = 3;
+    float cout1, cout2, cin = 3;
 
-    cout = sqrt1(cin);
+    cout1 = sqrt1(cin);
+    cout2 = sqrt2(cin);
 
-    printf("%f", cout);
+    printf("%f %f \n", cout1, cout2);
 }
